b5a4d8de840f6b679d49eed77d47b2e9d8877b94
7sgn <sette562@gmail.com>
2024-11-01 18:17:03 -0300

    Commit 01/11/2024 18:16

--- DevBurger-Api/src/Routes.js
+++ DevBurger-Api/src/Routes.js
@@ -1,10 +1,19 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
 
+import User from './app/models/Users.js'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-    return response.status(200).json({message: 'Deu bom'})
+routes.get('/', async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'Marlon',
+        email: 'sette562@gmail.com',
+        passworld_hash: '12345'
+
+    })
+    return response.status(200).json({user})
 
 })
 
